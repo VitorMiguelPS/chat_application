4e1848725b9888ceab4dbfe183ee06a438aa7510
Vitor Miguel <projetos09@penseavanti.com.br>
2021-02-17 17:23:57 -0300

    Finishing the application

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine } from "react-chat-engine";
 
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 
 import "./assets/App.css";
 
 const App = () => {
+  if(!localStorage.getItem("username")) return <LoginForm />
+
   return (
     <ChatEngine 
         height="100vh"
         projectID="55bb295a-dba7-4061-92f5-680bbe00bcb8"
-        userName="vitormiguelmaster"
-        userSecret="123456"
+        userName={localStorage.getItem("username")}
+        userSecret={localStorage.getItem("password")}
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
